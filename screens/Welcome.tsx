7da59fa7a58acd84b0fdc7e8b9ebbe8cf29fae7c
OKORO KELVIN <okorokelvinemoakpooke@yahoo.com>
2022-07-29 10:42:10 +0100

    Completed code

--- screens/Welcome.tsx
+++ screens/Welcome.tsx
@@ -1,66 +1,34 @@
-import React, { FunctionComponent } from "react";
+import React from "react";
 import { StatusBar } from "expo-status-bar";
 import styled from "styled-components/native";
-import { Container } from "../components/shared";
+
+// custom components
 import { colors } from "../components/colors";
-import background from "./../assets/bgs/background_h1.png";
-import BigText from "../components/Texts/BigText";
-import SmallText from "../components/Texts/SmallText";
-import RegularText from "../components/Texts/RegularText";
-import RegularButton from "../components/Buttons/RegularButton";
+import { Container } from "../components/shared";
+import CardSection from "../components/Cards/CardSection";
+import TransactionSection from "../components/Transactions/TransactionSection";
+import MoneyBottomSheet from "../components/SendMoney/MoneyBottomSheet";
+
+import { RootStackParamList } from "../navigators/RootStack";
+import { StackScreenProps } from "@react-navigation/stack";
 
 const WelcomeContainer = styled(Container)`
-background-color: ${colors.secondary};
-justify-content: space-between;
-width: 100%;
-height:100%;
-flex: 1;
+  background-color: ${colors.graylight};
+  width: 100%;
+  flex: 1;
 `;
 
-const TopImage = styled.Image`
-width: 100%;
-height:100%;
-resize-mode: stretch
-`;
+type Props = StackScreenProps<RootStackParamList, "Welcome">;
 
-const TopSection = styled.View`
-width:100%;
-max-height:55%;
-flex:1;
-`;
-
-const BottomSection = styled.View`
-width:100%;
-padding:25px;
-flex:1;
-justify-content:flex-end;
-`;
-
-
-
-const Welcome: FunctionComponent =()=>{
-    
-    return (
-        <>
-         <StatusBar style="light"/>
-         <WelcomeContainer>
-            <TopSection>
-                <TopImage source={background}/>
-            </TopSection>
-            <BottomSection>
-                <BigText textStyles={{width:"70%", marginBottom :25}}>
-                    Best way to track your money 
-                </BigText>
-                <SmallText textStyles={{width:"70%", marginBottom :25}}>
-                    Best payment method, connects your money to your friends from here 
-                </SmallText>
-                <RegularButton onPress={()=>{}}>
-                    Get Started 
-                </RegularButton>
-            </BottomSection>
-         </WelcomeContainer>
-        </>
-    );
+const Welcome = ({ navigation }: Props) => {
+  return (
+    <WelcomeContainer>
+      <StatusBar style="dark" />
+      <CardSection navigation={navigation} />
+      <TransactionSection />
+      <MoneyBottomSheet />
+    </WelcomeContainer>
+  );
 };
 
 export default Welcome;
